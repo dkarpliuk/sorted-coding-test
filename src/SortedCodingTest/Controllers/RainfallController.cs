3066aca152c86deeeac889b1187500dd5ba2de35
Dmytro Karpliuk <d.karpliuk@geniusee.com>
2023-11-27 16:34:20 +0100

    Added RainfallController

---
+++ src/SortedCodingTest/Controllers/RainfallController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SortedCodingTest.Models;
+using SortedCodingTest.Services.Interfaces;
+using SortedCodingTest.Services.Models;
+
+namespace SortedCodingTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RainfallController : ControllerBase
+    {
+        private readonly IRainfallService _rainfallService;
+
+        public RainfallController(IRainfallService rainfallService)
+        {
+            _rainfallService = rainfallService ?? throw new ArgumentNullException(nameof(rainfallService));
+        }
+
+        [HttpGet(Name = "GetLatestStationReadings")]
+        [Route("id/{stationId}/readings")]
+        public async Task<RainfallReadingResponse> GetLatestStationReadingsAsync(LatestStationReadingsRequest request)
+        {
+            return await _rainfallService.GetLatestStationReadingsAsync(request.StationId, request.Maximum);
+        }
+    }
+}
---
+++ src/SortedCodingTest/Models/LatestStationReadingsRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace SortedCodingTest.Models
+{
+    public class LatestStationReadingsRequest
+    {
+        private const int MaxStationReadingsLimit = 10000;
+
+        [FromRoute]
+        public int StationId { get; set; }
+
+        [FromQuery]
+        public int Minimum { get; set; }
+
+        [FromQuery]
+        [Range(0, MaxStationReadingsLimit)]
+        public int Maximum { get; set; }
+    }
+}
