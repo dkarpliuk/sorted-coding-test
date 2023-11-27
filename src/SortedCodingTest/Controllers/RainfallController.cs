a4e537a76659acd053ce54876647dfab653e5745
Dmytro Karpliuk <d.karpliuk@geniusee.com>
2023-11-27 19:03:15 +0100

    Configure swagger

--- src/SortedCodingTest/Controllers/RainfallController.cs
+++ src/SortedCodingTest/Controllers/RainfallController.cs
@@ -18,6 +18,10 @@ namespace SortedCodingTest.Controllers
 
         [HttpGet]
         [Route("id/{stationId}/readings")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesErrorResponseType(typeof(ErrorResponse))]
         public async Task<RainfallReadingResponse> GetLatestStationReadingsAsync([FromRoute, FromQuery] LatestStationReadingsRequest request)
         {
             return await _rainfallService.GetLatestStationReadingsAsync(request.StationId, request.Maximum);
